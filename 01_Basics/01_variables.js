bede0d2df30bc06360fa07c752635512564496ce
aman <raushan.aman54@gmail.com>
2025-01-28 21:34:43 +0530

    Create 01_variables.js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+// ways of creating variables in javascript
+const account_id = 12345
+let accountEmail= "abc@gmail.com"
+var pass="123456"
+//account_id = 2 //not allowed
+console.log(account_id) 
+accountEmail = "hc@gmail.com"
+pass = "5432"
+console.table([account_id,accountEmail,pass])
+
+/*prefer not to var keyword declaring variables in javascript 
+because of issue in block scope and functional scope*/
+
