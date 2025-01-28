95af829de426c647fb57653cb0ea920ef8c0c814
aman <raushan.aman54@gmail.com>
2025-01-28 21:35:14 +0530

    Create 02_datatypes.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+// alert (3+3) we are using nodejs, not browser
+
+// datatypes in javascript
+
+let name = "aman" //string datatype
+let age = 70 //number datatype
+let isLogin = false // boolean datatype
+
+/*primitive datatype*/
+
+// number => 2 to power 53
+// bigint 
+//string = ""
+// boolean = true or false
+//Null = > it's a standalone value
+// undefined => 
+// symbol => unique
+
+/* Object datatype*/
+
+//typeof
+
+//type of null is object in javascript
