5232d1b59cadaf52da4ca66994c9a9bc7a6b0b63
aman <raushan.aman54@gmail.com>
2025-02-01 22:51:43 +0530

    javascript datatypes

---
+++ 01_Basics/03_datatype_conversion.js
@@ -0,0 +1,21 @@
+// let score = "333abs"
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+//888******************************************
+
+// let a = 3
+// let b = -3
+// let neg1 = -a
+// let neg2 = -b
+// console.log(neg1)
+// console.log(neg2)
+
+//******************concatination************ */
+
+let str1 = "aman"
+let str2 = " Raushan"
+str3 = str1 + str2
+console.log(str3)
---
+++ 01_Basics/05_datatypes.js
@@ -0,0 +1,22 @@
+/* primitive datatypes
+ --> 7 types: String, Number, null, Boolean, undefined, symbol,BigInt
+
+Note--> 
+
+# Reference datatype(Non - primitive datatypes)
+ --> array , objects,functions */
+ //Array example
+ const heroes= ["rohit","virat","jadeja"]
+
+ //Object example
+let variable = {
+    name:"aman",
+    age:20,
+ }
+
+ //function example
+//Note--> we can also store function into a variable
+ let function_variable=function(){
+    console.log("hello aman");
+    
+ }
