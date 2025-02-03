2863859db644a578bb5a15d36055a05225c2a9d5
aman <raushan.aman54@gmail.com>
2025-02-03 22:51:29 +0530

    Create 08_Numbers_Math.js

---
+++ 01_Basics/08_Numbers_Math.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+// convert it into string
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hundred = 10000000
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
+
+// ***************************************** About Maths **************************************************
+
+// learn maths through online content for better understanding
