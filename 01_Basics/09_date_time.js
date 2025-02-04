3a4c359e221ffb704007754619691a96536ecc72
aman <raushan.aman54@gmail.com>
2025-02-04 22:19:55 +0530

    Create 09_date_time.js

---
+++ 01_Basics/09_date_time.js
@@ -0,0 +1,42 @@
+// Dates
+
+// let myDate = new Date()
+//  console.log(myDate.toISOString());
+//  console.log(myDate.toJSON())
+//  console.log(myDate.toLocaleDateString())
+//  console.log(myDate.toLocaleTimeString());
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toString());
+
+//  let createdDate = new Date(2023,0,23)
+//  console.log(createdDate.toDateString());
+
+
+ // date under specific format such yyyy|mm|dd
+
+//  let createdDate = new Date("01-31-2025")
+//  console.log(createdDate.toLocaleString())
+
+//  let myTimestamp = Date.now()
+//  console.log(myTimestamp);
+
+//  let createdDate = new Date("01-31-2025")
+//   console.log(createdDate.getTime())
+
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+
+newDate.toLocaleString('default', {
+    weekday:  "long",
+    
+})
+
+
+
+ 
+ 
