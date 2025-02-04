c20904b54d29a8b074c311c240da57cf37bb3ccc
aman <raushan.aman54@gmail.com>
2025-02-04 22:20:14 +0530

    Create 10_array.js

---
+++ 01_Basics/10_array.js
@@ -0,0 +1,27 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myHero = ["naman", "aman","mukesh"]
+
+// array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(8)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(newArr);
+
+console.log(myArr);
+// learn array function with the mdn or any online websites.
