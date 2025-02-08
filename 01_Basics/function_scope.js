0f6c30901e34bdc8431b164500affa5154c6704d
aman <raushan.aman54@gmail.com>
2025-02-08 21:41:08 +0530

    Update function_scope.js

--- 01_Basics/function_scope.js
+++ 01_Basics/function_scope.js
@@ -1,6 +1,38 @@
-let a = 10
+if(true){
+    let a = 10
 const b = 20
 var c = 30
-console.log(a);
-console.log(b);
+
+}
+
+//console.log(a);
+//console.log(b);
 console.log(c);
+// here we can see that var c = 30 is declare inside if condition scope but it works also outside the scope 
+// which is not true. That's why we avoid declaring variable with var keyword
+
+
+function one(){
+    const username = "aman"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+
+    }
+    
+    two()
+    
+}
+one()
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++
+ console.log(addone(5))
+function addone(num){
+    return num +1
+ }
+ addone(5)
+
+ const addtwo = function(num){ // this way is also called  expression
+    return num + 2
+ }
+ addtwo(5)
