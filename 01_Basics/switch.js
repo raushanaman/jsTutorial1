c7e49f8e5f3b7a08e366d56b1aa7a6191b45726c
aman <raushan.aman54@gmail.com>
2025-02-10 21:56:10 +0530

    Update switch.js

--- 01_Basics/switch.js
+++ 01_Basics/switch.js
@@ -31,3 +31,28 @@ default:
 //         break;
 // }
 
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("february");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        break;
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
