ac0e789400a337c13d8d45a0fe67dee426b3913a
aman <raushan.aman54@gmail.com>
2025-02-11 18:43:13 +0530

    Create for.js

---
+++ 01_Basics/for.js
@@ -0,0 +1,23 @@
+const myObject = {
+    js:'javascript',
+    cpp:'c++',
+    rb:"ruby"
+}
+
+// for in loop
+for (const key in myObject) {
+    //if (Object.prototype.hasOwnProperty.call(myObject, key)) {
+       // const element = myObject[key];
+       // console.log(myObject[key]);
+        
+    //}
+}
+// for in loop in javascript
+
+const programming = ["js","rb","java","cpp"]
+
+for(const key in programming)
+{
+    console.log(programming[key]);
+    
+}
