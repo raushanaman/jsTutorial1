6a8a32d518489bf00e2795d7dc901709c2d5462b
aman <raushan.aman54@gmail.com>
2025-02-11 18:43:01 +0530

    Create for_each.js

---
+++ 01_Basics/for_each.js
@@ -0,0 +1,42 @@
+// for each loop
+
+const coding= ["js","cpp","java","c","python"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// } )
+
+// through arrow function
+// coding.forEach((item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr)=>{
+//     console.log(item,index,arr);
+    
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        
+    },
+
+]
+
+// how to access
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+    
+})
